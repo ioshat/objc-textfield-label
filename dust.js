5a78f67492543045aa505e44c588d6c0c60c1447
Tomas Ruzicka <tomasruzicka@abdoc.net>
2014-08-18 18:54:07 +0200

    init

---
+++ dust.js
@@ -0,0 +1,45 @@
+
+var filters = {
+
+};
+
+var helpers = {
+    objCColor: function(chunk, ctx, bodies, params) {
+      var color = ctx.get('color');
+      var f = function(n) { return n < 0.001 ? 0 : (n/255).toFixed(3); };
+      return chunk.write('TODO');
+      // return chunk.write('colorWithRed:'+f(color.r)+' green:'+f(color.g)+' blue:'+f(color.b)+' alpha:' + color.a);
+    },
+    objCAlignment: function(chunk, ctx, bodies, params) {
+      var align = ctx.get('textAlign');
+      var code = '';
+      switch (align) {
+        case 'left':
+          code = 'NSTextAlignmentLeft';
+          break;
+        case 'right':
+          code = 'NSTextAlignmentRight';
+          break;
+        case 'center':
+          code = 'NSTextAlignmentCenter';
+          break;
+        case 'justified':
+          code = 'NSTextAlignmentJustified';
+          break;
+      }
+      return chunk.write(code);
+    },
+    objCFont: function(chunk, ctx, bodies, params) {
+      var baseTextStyle = ctx.get('baseTextStyle');
+      return chunk.write(baseTextStyle.font.name);
+    },
+    nsLocalizedString: function(chunk, ctx, bodies, params) {
+      var loc = ctx.get('options')['localizedString'] ? true : false;
+      return chunk.write(loc ?
+        'NSLocalizedString(@"' + ctx.get('text') + '", nil)' :
+        '@"' + ctx.get('text') + '"');
+    }
+};
+
+exports.filters = filters;
+exports.helpers = helpers;
